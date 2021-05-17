e0921ad768a9046e490cb34b9a98bc35157cbdab
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-17 22:12:52 +0200

    Shapes SwiftUI

--- Shapes/ContentView.swift
+++ Shapes/ContentView.swift
@@ -9,8 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView(.vertical) {
+                Button(action:  {
+                    
+                }, label: {
+                    Text("Delete Account")
+                        .frame(width: 200, height: 50)
+                        .foregroundColor(Color.red)
+                       .background(Color.black)
+                        .clipShape(Capsule())
+                })
+                Circle()
+                    .frame(width: 150, height: 150)
+                    .foregroundColor(Color.blue)
+                Rectangle()
+                    .frame(width: 200, height: 150)
+                    .foregroundColor(Color.red)
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 200, height: 150)
+                    .foregroundColor(Color.green)
+                Capsule()
+                    .frame(width: 200, height: 50)
+                    .foregroundColor(Color.purple)
+                Ellipse()
+                    .frame(width: 100, height: 50)
+                    .foregroundColor(Color.yellow)
+                
+            }
+            .navigationTitle("Shapes")
+        }
     }
 }
 
