5aba8b9de061f4a7f059d5bf72da866caaff75a3
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-17 21:44:19 +0200

    Initial Commit

---
+++ Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by Ndayisenga Jean Claude on 17/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
